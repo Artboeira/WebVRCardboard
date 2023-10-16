98c594e8ca17953f3ce4b85548b8d1ec11387014
Arthur Boeira <39688792+Artboeira@users.noreply.github.com>
2023-10-16 06:29:03 -0300

    create basic structure of WebVR experience

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Scene
+const scene = new THREE.Scene();
 
-setupCounter(document.querySelector('#counter'))
+// Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 1.6, 3);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.xr.enabled = true; // Enable WebXR
+document.body.appendChild(VRButton.createButton(renderer)); // VR button for interaction
+document.body.appendChild(renderer.domElement);
+
+// Controls for Standard Navigation (non-VR) - Optional
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 1.6, 0);
+controls.update();
+
+// Creating a 360° panoramic view
+const geometry = new THREE.SphereGeometry(500, 60, 40);
+geometry.scale(-1, 1, 1); // Make the inside of the sphere the "outside"
+
+const texture = new THREE.TextureLoader().load('path_to_your_360_image.jpg'); // Put the path of your 360 image here
+const material = new THREE.MeshBasicMaterial({ map: texture });
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Animation loop
+function animate() {
+  renderer.setAnimationLoop(function () {
+    renderer.render(scene, camera);
+  });
+}
+
+// Handle window resize
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+animate(); // Start the animation loop
+
